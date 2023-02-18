301283298bc687b21f5f4707fd01d1acbe33ed47
escowin <edwin@escowinart.com>
2023-02-17 19:36:31 -0600

    21.1.6 | set up remaining type definitions & resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -9,10 +9,33 @@ const resolvers = {
     // ternary operates checks if username exists.
     // - if so, sets params to an object with user key-value.
     // - if not, returns an empty string.
+
+    // get all thoughts
     thoughts: async (parent, { username }) => {
       const params = username ? { username } : {};
       return Thought.find(params).sort({ createdAt: -1 });
     },
+
+    // get a thought
+    thought: async (paret, { _id }) => {
+        return Thought.findOne({ _id })
+    },
+
+    // get all users
+    users: async () => {
+        return User.find()
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts')
+    },
+
+    // get a user by username
+    user: async (parent, { username }) => {
+        return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts')
+    }
   },
 };
 
