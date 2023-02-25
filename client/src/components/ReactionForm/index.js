0591cf3aa2d655ecc3f7feb8da2932abc282700e
escowin <edwin@escowinart.com>
2023-02-25 11:32:00 -0600

    set up reaction form & corresponding mutation

---
+++ client/src/components/ReactionForm/index.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_REACTION } from "../../utils/mutations";
+
+const ReactionForm = ({ thoughtId }) => {
+  const [reactionBody, setBody] = useState("");
+  const [characterCount, setCharacterCount] = useState(0);
+
+  const [addReaction, { error }] = useMutation(ADD_REACTION);
+
+  const handleChange = (event) => {
+    if (event.target.value.length <= 200) {
+      setBody(event.target.value);
+      setCharacterCount(event.target.value.length);
+    }
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await addReaction({
+        variables: { reactionBody, thoughtId },
+      });
+
+      // clears form value
+      setBody("");
+      setCharacterCount(0);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div>
+      <p className={`m-0 ${characterCount === 280 ? "text-error" : ""}`}>
+        character count: {characterCount}/200
+        {/* conditional rendering */}
+        {error && <span className="ml-2">Something went wrong...</span>}
+      </p>
+
+      <form
+        className="flex-row justify-center justify-space-between-md align-stretch"
+        onSubmit={handleFormSubmit}
+      >
+        <textarea
+          placeholder="react to this thought"
+          className="form-input col-12 col-md-9"
+          value={reactionBody}
+          onChange={handleChange}
+        ></textarea>
+
+        <button className="btn col-12 col-md-3" type="submit">
+          submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ReactionForm;
